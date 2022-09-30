e83fa13a33a04c963e35a97c30f95e294a7cf5ad
ndthanh143 <nguyenduythanh421.tna@Gmail.com>
2022-09-30 15:38:39 +0700

    Build footer component

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,9 +1,11 @@
 import Header from '~/components/Layout/components/Header';
+import Footer from '~/components/Layout/components/Footer';
 function DefaultLayout({ children }) {
     return (
         <div>
             <Header />
             <div className="Container">{children}</div>
+            <Footer />
         </div>
     );
 }
